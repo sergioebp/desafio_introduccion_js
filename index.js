098e042f42993727ced7414e947af0b396f36cae
Sergio Baquero <sergioebp@gmail.com>
2024-06-12 16:26:23 -0400

    desafio completo, botones funcionales

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+cantidad = 0
+
+cantidadSpan = document.querySelector(".cantidad");
+cantidadSpan.innerHTML = cantidad
+
+total = 0
+
+totalSpan = document.querySelector(".valor-total");
+totalSpan.innerHTML = total
+
+if (cantidad == 0) {
+    document.querySelector("#menos").style.display = "none"
+}
+
+document.querySelector("#mas").onclick = function(){
+    cantidad = cantidad + 1;
+    cantidadSpan.innerHTML = cantidad;
+    total = cantidad * precio;
+    totalSpan.innerHTML = total;
+    if (cantidad > 0) {
+        document.querySelector("#menos").style.display = "inline-block"
+    }
+};
+
+document.querySelector("#menos").onclick = function(){
+    cantidad = cantidad - 1;
+    cantidadSpan.innerHTML = cantidad;
+    total = cantidad * precio;
+    totalSpan.innerHTML = total;
+    if (cantidad == 0) {
+        document.querySelector("#menos").style.display = "none"
+    }
+};
